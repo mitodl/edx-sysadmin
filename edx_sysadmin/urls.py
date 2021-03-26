1980899e426ce75158d21eb177636e93d3ca65e7
Umar Asghar <umar.asghar@arbisoft.com>
2021-03-26 16:09:08 +0500

    Add Git import and Gitlogs code (#8)
    * Add Gitlogs code
    
    * fixes
    
    * address feedback
    
    * revert back to xmodule CourseKey

--- edx_sysadmin/urls.py
+++ edx_sysadmin/urls.py
@@ -3,7 +3,7 @@ URLs for edx_sysadmin.
 """
 from django.conf.urls import url
 
-from edx_sysadmin.views import CoursesPanel, SysadminDashboardView, UsersPanel
+from edx_sysadmin.views import CoursesPanel, SysadminDashboardView, UsersPanel, GitLogs
 
 app_name = "sysadmin"
 
@@ -11,5 +11,8 @@ app_name = "sysadmin"
 urlpatterns = [
     url("^$", SysadminDashboardView.as_view(), name="sysadmin"),
     url(r"^courses/?$", CoursesPanel.as_view(), name="courses"),
+    url(r'^gitlogs/?$', GitLogs.as_view(), name="gitlogs"),
+    url(r'^gitlogs/(?P<course_id>.+)$', GitLogs.as_view(),
+        name="gitlogs_detail"),
     url(r"^users/$", UsersPanel.as_view(), name="users"),
 ]
