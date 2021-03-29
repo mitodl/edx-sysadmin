6e5011e53250bbcfde528b7b52fea239ff11196b
Umar Asghar <umar.asghar@arbisoft.com>
2021-03-29 11:23:46 +0500

    Umar/load new course from GitHub (#10)
    * add loading new courses from Github
    
    * address feedback

--- edx_sysadmin/urls.py
+++ edx_sysadmin/urls.py
@@ -3,7 +3,13 @@ URLs for edx_sysadmin.
 """
 from django.conf.urls import url
 
-from edx_sysadmin.views import CoursesPanel, SysadminDashboardView, UsersPanel, GitLogs
+from edx_sysadmin.views import (
+    CoursesPanel, 
+    SysadminDashboardView, 
+    UsersPanel, 
+    GitImport, 
+    GitLogs,
+)
 
 app_name = "sysadmin"
 
@@ -11,6 +17,7 @@ app_name = "sysadmin"
 urlpatterns = [
     url("^$", SysadminDashboardView.as_view(), name="sysadmin"),
     url(r"^courses/?$", CoursesPanel.as_view(), name="courses"),
+    url(r"^gitimport/$", GitImport.as_view(), name='gitimport'),
     url(r'^gitlogs/?$', GitLogs.as_view(), name="gitlogs"),
     url(r'^gitlogs/(?P<course_id>.+)$', GitLogs.as_view(),
         name="gitlogs_detail"),
